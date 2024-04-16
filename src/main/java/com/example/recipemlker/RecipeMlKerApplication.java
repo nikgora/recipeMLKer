942b58add9bdf8ec9c6bd15277da1bc052586ddd
Mykola Gora Jr <mykolagorajr@MacBook-Pro-von-Mykola.local>
2024-04-16 16:12:08 +0200

    Init commit

---
+++ src/main/java/com/example/recipemlker/RecipeMlKerApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipemlker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMlKerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeMlKerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipemlker/RecipeMlKerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipemlker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMlKerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
