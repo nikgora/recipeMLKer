54ed1e11e08f898ec9ca3d94e5360a157d884fbd
mykolagorajr <nik.gora.2004@gmail.com>
2024-04-19 10:27:41 +0200

    some changes in pages and controller

--- src/main/java/com/example/recipemlker/controller/UserController.java
+++ src/main/java/com/example/recipemlker/controller/UserController.java
@@ -1,12 +1,31 @@
 package com.example.recipemlker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import redis.clients.jedis.params.GetExParams;
 
 @Controller
 public class UserController {
     @GetMapping("/")
-    public String mainPage() {return "user/main";}
+    public String mainPage() {
+        return "user/main";
+    }
+
+    @GetMapping("/allRecipes")
+    public String allRecipePage() {
+        return "user/allRecipes";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "user/user";
+    }
+
     @GetMapping("/recipe")
-    public String recipePage() {return "user/recipe";}
+    public String recipePage(Model model, @RequestParam(value = "id") String id) {
+        model.addAttribute("id", id);
+        return "user/recipe";
+    }
 }
