20cd4ffc8d6798cb3eeda6eaa56e67ac9889720d
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2024-05-01 22:57:02 +0300

    made hovers with heart.svg and user.svg in header

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,13 @@
+function changeImage(name, newSrc) {
+    var img = document.getElementById(name);
+    if (img) {
+        img.src = newSrc;
+    }
+}
+
+function restoreImage(name, originalSrc) {
+    var img = document.getElementById(name);
+    if (img) {
+        img.src = originalSrc;
+    }
+}
