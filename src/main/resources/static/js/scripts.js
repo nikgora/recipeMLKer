a91f2345b04d9779e9fb699bae77174701446ec3
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2024-05-08 23:20:04 +0300

    added post method for login form in html and removed unnecessary code

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -35,28 +35,3 @@ document.getElementById("overlay-user").addEventListener("click",
     () => hideOverlay("overlay-user", "popup-login-window"));
 document.getElementById("confirm-button").addEventListener("click",
     () => hideOverlay("overlay-user", "popup-login-window"));
-
-document.getElementById('confirm-button').addEventListener('click', function() {
-    const login = document.querySelector('.login-form.email input').value;
-    const password = document.querySelector('.login-form.password input').value;
-
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ login, password })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log('Login successful!');
-                window.location.href = 'user.html';
-            } else {
-                console.log('Login failed:', data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error during login:', error);
-        });
-});
