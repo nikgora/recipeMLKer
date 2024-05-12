77b0b76aba1f84baac6c5d7d398c78d4782202ba
Liliia <kulabukhova.lilya@gmail.com>
2024-05-12 21:23:19 +0300

    <(。_。)>

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -25,3 +25,46 @@ function hideOverlay() {
 document.getElementById("support-btn").addEventListener("click", showOverlay);
 document.getElementById("overlay").addEventListener("click", hideOverlay);
 document.getElementById("send-button").addEventListener("click", hideOverlay);
+
+// Отримуємо елементи
+const dropDownIcon = document.querySelector('.DropDownIcon');
+const elementsToToggle = document.querySelectorAll('.substance, .mDIcon, .saladI, .pastryI, .dessertI, .smoothieI, .snackI, .fish, .sauceI');
+const timeI = document.querySelector('.timeI');
+
+// Додаємо обробник подій для кліка на DropDownIcon
+dropDownIcon.addEventListener('click', function() {
+    // Змінюємо видимість елементів при кліку
+    elementsToToggle.forEach(element => {
+        element.classList.toggle('hidden');
+    });
+
+    // Зсуваємо наступний елемент вниз
+    if (elementsToToggle[0].classList.contains('hidden')) {
+        timeI.style.marginTop = '20px'; // Задаємо менший відступ, коли елементи виїхали
+    } else {
+        timeI.style.marginTop = '60px'; // Задаємо більший відступ, коли елементи ховаються
+    }
+});
+
+// Отримуємо елементи
+const dropDownIcons = document.querySelectorAll('.DropDownIcon');
+
+// Додаємо обробник подій для кожного DropDownIcon
+dropDownIcons.forEach(dropDownIcon => {
+    dropDownIcon.addEventListener('click', function() {
+        // Знаходимо батьківський елемент li
+        const listItem = this.closest('li');
+
+        // Знаходимо елемент toggleBlock в межах батьківського li
+        const toggleBlock = listItem.querySelector('.toggleBlock');
+
+        // Змінюємо видимість блоку та тексту при кліку
+        toggleBlock.classList.toggle('hidden');
+
+        // Знаходимо елемент timeI в межах батьківського li
+        const timeI = listItem.querySelector('.timeI');
+
+        // Змінюємо відступ timeI в залежності від видимості toggleBlock
+        timeI.style.marginTop = toggleBlock.classList.contains('hidden') ? '20px' : '60px';
+    });
+});
