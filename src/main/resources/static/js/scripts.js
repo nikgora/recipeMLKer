5ea4ef0bd1ec3e5c96604aa15c53bfe9e63ab408
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2024-05-13 18:33:30 +0300

    created button for naming new list of recipes on user page

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -22,6 +22,7 @@ function hideOverlay(overlayId, windowId) {
     document.getElementById(windowId).style.display = "none";
 }
 
+//support button
 document.getElementById("support-btn").addEventListener("click",
     () => showOverlay("overlay-support", "popup-window"));
 document.getElementById("overlay-support").addEventListener("click",
@@ -29,10 +30,23 @@ document.getElementById("overlay-support").addEventListener("click",
 document.getElementById("send-button").addEventListener("click",
     () => hideOverlay("overlay-support", "popup-window"));
 
+//create a new list
+document.getElementById("createList-btn").addEventListener("click", function() {
+    showOverlay("overlay-list", "list-field-popup-window");
+});
+document.getElementById("confirm-list-btn").addEventListener("click", function() {
+    hideOverlay("overlay-list", "list-field-popup-window");
+});
+document.getElementById("overlay-list").addEventListener("click", function() {
+    hideOverlay("overlay-list", "list-field-popup-window");
+});
+
+//user login
 document.getElementById("user-btn1").addEventListener("click",
     () => showOverlay("overlay-user", "popup-login-window"));
 document.getElementById("overlay-user").addEventListener("click",
     () => hideOverlay("overlay-user", "popup-login-window"));
+
 
 
 const container = document.getElementById('recipeContainer');
