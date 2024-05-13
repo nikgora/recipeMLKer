ad0445a1685f695ab768091d269e32822228eb39
nikgora <nik.gora.2004@gmail.com>
2024-05-13 16:35:49 +0200

    Revert "PR"

--- src/main/java/com/example/recipemlker/service/UserReportService.java
+++
@@ -1,9 +0,0 @@
-package com.example.recipemlker.service;
-
-import com.example.recipemlker.model.UserReport;
-
-import java.util.List;
-
-public interface UserReportService {
-    List<UserReport> findAvailableForModer(boolean accepted, Long id);
-}
