33810a080dde3044dc88521e92b5931c6ec7dfde
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2024-05-14 02:55:57 +0300

    get rating from user on recipe page

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -23,26 +23,26 @@ function hideOverlay(overlayId, windowId) {
 }
 
 document.getElementById("support-btn").addEventListener("click",
-    () => showOverlay("overlay-support", "popup-window"));
-document.getElementById("overlay-support").addEventListener("click",
-    () => hideOverlay("overlay-support", "popup-window"));
+    () => showOverlay("overlay", "popup-window"));
+document.getElementById("overlay").addEventListener("click",
+    () => hideOverlay("overlay", "popup-window"));
 document.getElementById("send-button").addEventListener("click",
-    () => hideOverlay("overlay-support", "popup-window"));
+    () => hideOverlay("overlay", "popup-window"));
 
-document.getElementById("user-btn1").addEventListener("click",
+/*document.getElementById("user-btn1").addEventListener("click",
     () => showOverlay("overlay-user", "popup-login-window"));
 document.getElementById("overlay-user").addEventListener("click",
-    () => hideOverlay("overlay-user", "popup-login-window"));
+    () => hideOverlay("overlay-user", "popup-login-window"));*/
 
 
 const container = document.getElementById('recipeContainer');
 
-recipes.forEach((recipe, index) => {
+/*recipes.forEach((recipe, index) => {
     const field = document.createElement('div');
     field.classList.add('smallerField', 'smallerField-down' + (index + 1));
 
     container.appendChild(field);
-});
+});*/
 
 // Отримуємо елементи
 const dropDownIcon = document.querySelector('.DropDownIcon');
