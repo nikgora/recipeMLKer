23e241e31a3d3963b4cb43e00f6e6f1fe80d573c
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2024-05-20 23:46:22 +0300

    added favorite recipes into heart overlay

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -31,6 +31,11 @@ function validateInput(inputId) {
     return true;
 }
 
+document.getElementById("heart-btn").addEventListener("click",
+    () => showOverlay("overlay-favorite", "favorite-popup-window"));
+document.getElementById("overlay-favorite").addEventListener("click",
+    () => hideOverlay("overlay-favorite", "favorite-popup-window"));
+
 document.getElementById("support-btn").addEventListener("click",
     () => showOverlay("overlay", "popup-window"));
 document.getElementById("overlay").addEventListener("click",
